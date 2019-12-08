eef2fe9180dbcbf24d4d8debbe85b5610accc712
Kyle <kgbostian@gmail.com>
2019-12-08 17:01:13 -0600

    Cleaned up all the dead prints.

--- 2019/py/3day/day3.py
+++ 2019/py/3day/day3.py
@@ -21,7 +21,6 @@ def calc_point(direction, distance, point):
     else:
         raise Exception
     new_point = point.copy()
-    #print(f"New Point: {new_point}")
     return new_point
 
 def convert_to_points(movements):
@@ -35,12 +34,10 @@ def convert_to_points(movements):
         distance = int(action[1:])
         new_point = calc_point(direction, distance, point)
         all_points.append(new_point)
-    #print(f"Printing all points: {all_points}")
     return all_points 
 
 def manhattan_distance(R):
     md = []
-    #print(f"R is: {R}")
     if R == None:
         return "Failure to find MD"
     for x in R:
@@ -53,7 +50,6 @@ def parse_lines(lines):
     return red, blue
 
 def find_pts_seg(p1, p2):
-    #print(f"Two pts are: {p1}, {p2}")
     seg_pts = []
     if p1[0] == p2[0]:
         # X's are the same
@@ -61,38 +57,27 @@ def find_pts_seg(p1, p2):
             #increasing num
             for x in range(p2[1] - p1[1]):
                 seg_pts.append((p1[0], p1[1] + x))
-            #print(f"{seg_pts}")    
         elif p1[1] > p2[1]:
             #decreasing num
             for x in range(p1[1] - p2[1]):
                 seg_pts.append((p1[0], p1[1] - x))
-            #print(f"{seg_pts}")
     elif p1[1] == p2[1]:
         # Y's are the same
         if p1[0] < p2[0]:
             #increasing num
             for x in range(p2[0] - p1[0]):
                 seg_pts.append((p1[0] + x, p1[1]))
-            #print(f"{seg_pts}")
         elif p1[0] > p2[0]:
             #decreasing num
             for x in range(p1[0] - p2[0]):
                 seg_pts.append((p1[0] - x, p1[1]))
-            #print(f"{seg_pts}")
     return seg_pts            
 
 def flatten(l1):
-    # print()
-    # print()
-    # print(f"about to flatten: {l1}")
-    # print()
-    # print()
     out = []
     for x in l1:
-        #print(f"Printing x: {x}")
         for y in x:
             out.append(y)
-    #print(out)
     return out
 
 def find_all_pts(points):
@@ -100,40 +85,26 @@ def find_all_pts(points):
     for pt in range(len(points) - 1):
        all_line_pts.append(find_pts_seg(points[pt], points[pt+1]))
     return all_line_pts
-    #print(f"{all_line_pts}")    
 
 def find_intersection_pts(red, blue):
-    #print(red)
-    #print(blue)
     x_pts = []
     for x in red:
-        #print(f"Testing pt: {x}")
         if blue.count(x) > 0:
             x_pts.append(x)
-            #print(f"Found intersection: {x}")
 
 def part1(filename="input.data"):
     lines = read_file(filename)
     red,blue = parse_lines(lines)
     all_red_points = find_all_pts(red)
     all_red_points = flatten(all_red_points)
-    #print(f"All Red Points: {all_red_points}")
     all_blue_points = find_all_pts(blue)
     all_blue_points = flatten(all_blue_points)
     red_set = set(all_red_points)
     blue_set = set(all_blue_points)
     x_pts = red_set.intersection(blue_set)
-    #print(f"Using set for intersections: {x_pts}")
     md = manhattan_distance(x_pts)
     set_md = set(md)
     set_md.remove(0)
-    print(set_md)
     print(min(set_md))
-    #x_pts = find_intersection_pts(all_red_points,all_blue_points) 
-    #print(manhattan_distance(x_pts))
-    
-    #print(len(all_red_points))
-    #print(len(all_blue_points))
-    #print(f"Minimum distance = {min(md)}")
 
 part1("input.data")
